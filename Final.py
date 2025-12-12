9cea35d0872133e9b0f5071331c63f475980de6b
Benjamin Corcoran <74222105+corcoranbenjamin@users.noreply.github.com>
2025-12-12 16:09:22 -0500

    Fix notebook heading formatting and update Final.py comments

--- Final.py
+++ Final.py
@@ -1,7 +1,7 @@
 
 import numpy as np #numpy for all math 
 import matplotlib.pyplot as plt #matplotlib is for plotting
-from matplotlib.patches import Circle, Polygon #brings in circle and polygon for the plot
+from matplotlib.patches import Circle #brings in circle and polygon for the plot
 from scipy.integrate import odeint #for ODE simulation
 
 #************************ VARIABLES ***************************
